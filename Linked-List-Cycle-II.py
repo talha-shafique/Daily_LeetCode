c979d3311a933deae0ff6d45b081debe79a36ad0
Talha Shafique <talha.shafique126@gmail.com>
2025-05-09 23:49:02 +0500

    [142] [Time Beats: 54.69%] [Memory Beats: 95.15%] - LeetPush

---
+++ Linked-List-Cycle-II.py
@@ -0,0 +1,30 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow=head
+        fast=head
+        while fast and fast.next:
+            slow=slow.next
+            fast=fast.next.next
+            if slow==fast:
+                slow=head
+                while slow!=fast:
+                    slow=slow.next
+                    fast=fast.next
+                return slow
+        return None
+        
+                # while fast and fast.next:
+                #     slow=slow.next
+                #     fast=fast.next
+                #     if slow==fast:
+                #         return fast
+                # the codition I used (fast anf fast.next) is to check if  a cycle exists or not which i already have done so dont need this   condiiton to locate the origin
+                
+            
+            
