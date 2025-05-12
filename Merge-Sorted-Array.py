454ba2d37e61c7c84b4a322309595f5ed3f89a3e
Talha Shafique <talha.shafique126@gmail.com>
2025-05-13 03:06:35 +0500

    [88] [Time Beats: 100.00%] [Memory Beats: 71.61%] - LeetPush

--- Merge-Sorted-Array.py
+++ Merge-Sorted-Array.py
@@ -1,8 +1,8 @@
 class Solution:
     def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-        \\\
+        """
         Do not return anything, modify nums1 in-place instead.
-        \\\
+        """
         while n>0:
             nums1.pop()
             n=n-1
